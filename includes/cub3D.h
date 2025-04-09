01bf9eab455d8931eefd79d8d0da2b4b4769ef55
Lukas Stefanek <lstefane@c2r4p9.42vienna.com>
2025-04-09 16:12:26 +0200

    fixed header

--- includes/cub3D.h
+++ includes/cub3D.h
@@ -5,53 +5,40 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: lstefane <lstefane@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/07 11:44:31 by lstefane          #+#    #+#             */
-/*   Updated: 2025/04/09 11:42:08 by lstefane         ###   ########.fr       */
+/*   Created: 2025/04/09 16:11:12 by lstefane          #+#    #+#             */
+/*   Updated: 2025/04/09 16:12:14 by lstefane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
 # define CUB3D_H
 
-// STD-HEADERS
+//STD - HEADER
 # include <errno.h>
 # include <fcntl.h>
 # include <limits.h>
 # include <math.h>
 # include <stdbool.h>
-# include <stdio.h>
-# include <stdint.h>
-# include <stdlib.h>
-# include <unistd.h>
-
-//CUSTOM-HEADERS
-# include "structs.h"
-# include "constants.h"
-# include "controls.h"
-# include "debug.h"
 # include <stdint.h>
 # include <stdio.h>
 # include <stdlib.h>
 # include <sys/time.h>
 # include <unistd.h>
 
-// CUSTOM-HEADERS
-# include "constants.h"
+//CUSTOM - HEADER
 # include "structs.h"
-# include "enums.h"
+# include "constants.h"
 # include "controls.h"
+# include "debug.h"
+# include "enums.h"
 # include "keys.h"
 # include "libft.h"
+# include "math_utils.h"
 # include "mlx.h"
 # include "mlx_utils.h"
 # include "parsing.h"
-# include "utils.h"
-# include "X11/X.h"
-# include "math_utils.h"
 # include "render.h"
 # include "utils.h"
-# include "mlx.h"
-# include "mlx_utils.h"
 # include "X11/X.h"
 
 #endif // CUB3d_H
---
+++ includes/error_handling.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   error_handling.h                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lstefane <lstefane@student.42vienna.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/09 16:08:26 by lstefane          #+#    #+#             */
+/*   Updated: 2025/04/09 16:09:01 by lstefane         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ERROR_HANDLING_H
+#define ERROR_HANDLING_H
+
+void	free_all(t_game *game);
+void	free_all_exit(t_game *game);
+
+#endif //ERROR_HANDLING_H
