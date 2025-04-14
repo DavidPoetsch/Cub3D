f8b91e921144b7e3e566ae1654161792ed986dc6
Lukas Stefanek <lstefane@c2r4p9.42vienna.com>
2025-04-14 10:24:20 +0200

    update valgrind

--- srcs/error_handling/error_handling.c
+++ srcs/error_handling/error_handling.c
@@ -6,15 +6,28 @@
 /*   By: lstefane <lstefane@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/09 16:04:19 by lstefane          #+#    #+#             */
-/*   Updated: 2025/04/09 16:27:57 by lstefane         ###   ########.fr       */
+/*   Updated: 2025/04/14 10:24:08 by lstefane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3D.h"
 
+void free_textures(t_game *game)
+{
+	if (game->map.NO_tex)
+		free(game->map.NO_tex);
+	if (game->map.SO_tex)
+		free(game->map.SO_tex);
+	if (game->map.WE_tex)
+		free(game->map.WE_tex);
+	if (game->map.EA_tex)
+		free(game->map.EA_tex);
+}
+
 void	free_all(t_game *game)
 {
 	free_mlx(game);
+	free_textures(game);
 	if (game->map.lst)
 		clear_map_lst(&game->map.lst);
 	if (game->map.arr)
