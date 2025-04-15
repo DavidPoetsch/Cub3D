464a0cdba941c3986f1e2647b73c1e5e9b561655
Lukas Stefanek <lstefane@c2r4p9.42vienna.com>
2025-04-15 15:09:29 +0200

    map parsing changes

--- includes/utils.h
+++ includes/utils.h
@@ -6,7 +6,7 @@
 /*   By: lstefane <lstefane@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/08 08:59:12 by dpotsch           #+#    #+#             */
-/*   Updated: 2025/04/15 14:43:59 by lstefane         ###   ########.fr       */
+/*   Updated: 2025/04/15 15:08:53 by lstefane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,7 @@ bool	is_collision(char **map, int x, int y);
 bool	is_inbounds(char **map, int x, int y);
 bool	is_door(char **map, int x, int y);
 bool	is_player(char c);
+bool is_valid_map_char(char c);
 
 //PIXEL UTILS
 t_pixel	new_pxl(int x, int y, int color);
--- srcs/utils/map_utils.c
+++ srcs/utils/map_utils.c
@@ -6,7 +6,7 @@
 /*   By: lstefane <lstefane@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/15 13:57:10 by lstefane          #+#    #+#             */
-/*   Updated: 2025/04/15 15:07:20 by lstefane         ###   ########.fr       */
+/*   Updated: 2025/04/15 15:09:12 by lstefane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,11 @@ bool	is_player(char c)
 
 bool is_valid_map_char(char c)
 {
-	if (!is_player)
+	if (!is_player(c))
 		return (false);
+	if (c != WALL && c != OPEN)
+		return (false);
+	if (c != DOOR) //BONUS
+		return (false);
+	return (true);
 }
