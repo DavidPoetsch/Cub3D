341fc4ec78b4f394c5e54ccfcfd2719aaaf18fc7
David Poetsch <poetschdavi@gmail.com>
2025-04-17 17:08:22 +0200

    update pos after dead

--- srcs/multiplayer/check_player_state.c
+++ srcs/multiplayer/check_player_state.c
@@ -6,7 +6,7 @@
 /*   By: dpotsch <poetschdavid@gmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/17 16:30:02 by dpotsch           #+#    #+#             */
-/*   Updated: 2025/04/17 16:57:34 by dpotsch          ###   ########.fr       */
+/*   Updated: 2025/04/17 17:07:11 by dpotsch          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,9 +51,31 @@ void	check_player_alive(t_game *game)
 	}
 }
 
+static void	set_player_alive()
+{
+	int fd;
+	char *file;
+
+	file = "./test/player_state.txt";
+	fd = open(file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (fd == -1)
+		return ;
+	write(fd, "alive\n", 6);
+}
+
+void	reset_player_pos(t_game *game)
+{
+	game->player.pos.x = game->player.start_x;
+	game->player.pos.y = game->player.start_y;
+	set_player_alive();
+	game->player.alive = true;
+}
+
 void	check_player_state(t_game *game)
 {
 	set_enemy_dead(game);
 	if (game->player.alive)
 		check_player_alive(game);
+	else
+		reset_player_pos(game);
 }
