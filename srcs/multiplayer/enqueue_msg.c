a8cc9870aa6d6d202fc2764f87a7db786cabf9fe
David Poetsch <poetschdavi@gmail.com>
2025-04-24 13:04:11 +0200

    changed bug with door

--- srcs/multiplayer/enqueue_msg.c
+++ srcs/multiplayer/enqueue_msg.c
@@ -6,7 +6,7 @@
 /*   By: dpotsch <poetschdavid@gmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/22 14:40:33 by dpotsch           #+#    #+#             */
-/*   Updated: 2025/04/24 11:42:59 by dpotsch          ###   ########.fr       */
+/*   Updated: 2025/04/24 12:43:29 by dpotsch          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,4 @@ void	enqueue_msg(t_snd_rcv *sr, char *msg)
 	sr->i_buf++;
 	ft_bzero(&sr->msg_lst[sr->i_buf], sizeof(MSG_SIZE));
 	ft_strlcpy(sr->msg_lst[sr->i_buf], msg, MSG_SIZE);
-	ft_printf("enqueue msg: %s", msg);
 }
