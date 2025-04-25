2a2ec5d0e5e77c177f8d469d244eb17a7952c358
Lukas Stefanek <lstefane@c2r4p9.42vienna.com>
2025-04-25 15:14:00 +0200

    added file check

--- bonus/srcs/parsing/parsing.c
+++ bonus/srcs/parsing/parsing.c
@@ -3,20 +3,34 @@
 /*                                                        :::      ::::::::   */
 /*   parsing.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dpotsch <poetschdavid@gmail.com>           +#+  +:+       +#+        */
+/*   By: lstefane <lstefane@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/07 13:46:37 by lstefane          #+#    #+#             */
-/*   Updated: 2025/04/25 16:04:12 by dpotsch          ###   ########.fr       */
+/*   Updated: 2025/04/25 16:50:08 by lstefane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3D.h"
+
+static int check_file_format(char *file)
+{
+	char *file_ending;
+
+	file_ending = ft_strrchr(file, '.');
+	if (ft_strcmp(file_ending, ".cub"))
+		return (SUCCESS);
+	ft_eprintf("Error: invalid file format (%s)\n", file_ending);
+	return (ERROR);
+}
 
 int	parse_cub_file(t_game *game, char *file)
 {
 	int	fd;
 	int	res;
 
+	res = check_file_format(file);
+	if (res != SUCCESS)
+		return (ERROR);
 	fd = open(file, O_RDONLY);
 	if (fd == FAILED)
 	{
