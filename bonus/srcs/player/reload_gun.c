14808d14951267d54925d2113170adc88a2cdc34
Lukas Stefanek <lstefane@c2r4p9.42vienna.com>
2025-04-28 09:26:16 +0200

    added reload block when full ammo

--- bonus/srcs/player/reload_gun.c
+++ bonus/srcs/player/reload_gun.c
@@ -6,7 +6,7 @@
 /*   By: lstefane <lstefane@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/24 16:16:32 by lstefane          #+#    #+#             */
-/*   Updated: 2025/04/24 16:24:21 by lstefane         ###   ########.fr       */
+/*   Updated: 2025/04/28 09:25:16 by lstefane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	reload_gun(t_game *game)
 {
-	if (game->player.reloads == 1)
+	if (game->player.reloads == 1 && game->player.ammo != AMMO_MAX)
 	{
 		game->player.reloads = 0;
 		game->player.ammo = AMMO_MAX;
