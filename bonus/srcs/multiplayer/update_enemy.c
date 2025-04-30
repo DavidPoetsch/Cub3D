3aebfe0ca9ecb09e25db17ed19074a865e06dffd
Lukas Stefanek <lstefane@c2r4p9.42vienna.com>
2025-04-30 13:36:32 +0200

    update enemy pos correctly

--- bonus/srcs/multiplayer/update_enemy.c
+++ bonus/srcs/multiplayer/update_enemy.c
@@ -6,7 +6,7 @@
 /*   By: lstefane <lstefane@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/17 13:53:17 by lstefane          #+#    #+#             */
-/*   Updated: 2025/04/24 14:22:26 by lstefane         ###   ########.fr       */
+/*   Updated: 2025/04/30 13:33:08 by lstefane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,6 @@ void	update_enemy_pos(t_game *game)
 	game->enemy.grid.y = floor(game->enemy.pos.y);
 	if (is_enemy(game->map.arr, game->enemy.grid_old.x, game->enemy.grid_old.y))
 		game->map.arr[game->enemy.grid_old.y][game->enemy.grid_old.x] = OPEN;
-	if (is_floor(game->map.arr, game->enemy.grid.x, game->enemy.grid.y))
+	if (!is_collision(game->map.arr, game->enemy.grid.x, game->enemy.grid.y))
 		game->map.arr[game->enemy.grid.y][game->enemy.grid.x] = ENEMY;
 }
