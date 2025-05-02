70adae28e9d70a1c6a4a1fdcb41e9c9613f88b3d
Lukas Stefanek <lstefane@c2r4p9.42vienna.com>
2025-05-02 15:12:42 +0200

    merge

--- bonus/srcs/parsing/parse_tex_lst.c
+++ bonus/srcs/parsing/parse_tex_lst.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parse_tex_lst.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dpotsch <poetschdavid@gmail.com>           +#+  +:+       +#+        */
+/*   By: lstefane <lstefane@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/18 11:59:38 by lstefane          #+#    #+#             */
-/*   Updated: 2025/05/02 15:08:25 by dpotsch          ###   ########.fr       */
+/*   Updated: 2025/05/02 15:12:15 by lstefane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
